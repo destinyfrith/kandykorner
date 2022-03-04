96fa802f5f6cd3299aaff0ce50ee08fa6fa1a651
Destiny Frith <desilyvahn@gmail.com>
2022-03-04 14:14:26 -0600

    css changes

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
+import "./KandyKorner.css"
 
 export const KandyKorner = () => {
     return (
