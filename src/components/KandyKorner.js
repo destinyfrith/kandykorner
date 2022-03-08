f37660792912ea103277f629f27c83ebf60ba670
Destiny Frith <desilyvahn@gmail.com>
2022-03-08 11:53:03 -0600

    added a login page

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,20 +1,36 @@
 import React from "react"
-import { Route } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import { NavBar } from "./nav/NavBar"
 import { ApplicationViews } from "./ApplicationViews"
 import "./KandyKorner.css"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
 
-export const KandyKorner = () => {
-    return (
+export const KandyKorner = () => (
         <>
-        <Route>
+        <Route
+            render={() => {
+                if (localStorage.getItem("kandy_customer")) {
+                    return (
+        <>
         <NavBar />
-        <h1>Kandy Korner</h1>
         <ApplicationViews />
-        </Route>
         </>
-    )
+                    )
+} else {
+    return <Redirect to="/login" />;
 }
+}}
+/>
+
+<Route path="/login">
+<Login />
+</Route>
+<Route path="/register">
+<Register />
+</Route>
+</>
+)
 
 
 // import { LocationList } from "./locations/LocationList"
